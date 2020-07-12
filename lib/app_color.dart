68cc9b9f4f088a28622cec61e0b809c12c4140ea
unknown <darmikawayan017@gmail.com>
2020-07-12 08:54:26 +0800

    uas wayan

---
+++ lib/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  final Color colorPrimary = Colors.lightBlue;
+  final Color colorSecondary = Colors.greenAccent;
+  final Color colorTertiary = Colors.pinkAccent;
+}
